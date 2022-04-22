9ced4ca10f3c1eb180cd977cbf193dfd2fe3b108
testliana <98384662+testliana@users.noreply.github.com>
2022-04-22 21:45:18 +0400

    add

--- srс/index.js
+++ srс/index.js
@@ -1,3 +1,84 @@
-import 'normalize.css'
+import 'normalize.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './styles/main.scss';
 import './index.html'
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+
+      var swiper = new Swiper(".offerSwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        modules: [Navigation, Pagination],
+        breakpoints: {
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+          },
+          768: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+          },
+          1000: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }
+      });
+
+      
+
+      var swiper = new Swiper(".trendsSwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        modules: [Navigation, Pagination],
+        breakpoints: {
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+          },
+          768: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+          },
+          1200: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }
+      });
+
+      var swiper = new Swiper(".categorySwiper", {
+        slidesPerView: "auto",
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
